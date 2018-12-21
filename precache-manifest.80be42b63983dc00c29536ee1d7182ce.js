98cced8921fe5d18e2817ee61982e31d8b4f12b5
Gabriella <csgabka@users.noreply.github.com>
2018-12-21 00:16:30 +0000

    Updates

---
+++ precache-manifest.80be42b63983dc00c29536ee1d7182ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9caa679ec445c7723001",
+    "url": "/React-calculator/static/js/runtime~main.9caa679e.js"
+  },
+  {
+    "revision": "d7d2198ed629ade29e64",
+    "url": "/React-calculator/static/js/main.d7d2198e.chunk.js"
+  },
+  {
+    "revision": "8cbaa72b2016a0fc94ef",
+    "url": "/React-calculator/static/js/1.8cbaa72b.chunk.js"
+  },
+  {
+    "revision": "d7d2198ed629ade29e64",
+    "url": "/React-calculator/static/css/main.8101306f.chunk.css"
+  },
+  {
+    "revision": "9dbd531ba16306a473b0e238c44f8480",
+    "url": "/React-calculator/index.html"
+  }
+];
